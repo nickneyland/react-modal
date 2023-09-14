7c8ff944385d633ed6a98399dfd704818cf855ba
Nick Neyland <nickneyland@users.noreply.github.com>
2023-09-14 11:01:02 -0400

    initial file upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="test 1"/>
+      <Todo text="test 2" />
+      <Todo text="test 3" />
+    </div>
+  );
+}
+
+export default App;
